4d553e27834a754b5336467b57a8b6f838b41f7e
Jose Storopoli <jose@storopoli.io>
2024-01-12 05:44:37 -0300

    feat(TUI): implement TUI for curl and wget
    Closes #2.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,32 @@
 use anyhow::Context;
-use axum::{routing::get, Router};
+use axum::{http::header::USER_AGENT, http::Request, response::Response, routing::get, Router};
 use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
 
 mod data;
 mod pages;
-use pages::{quote, root};
+use pages::{plain_quote, quote, root};
+
+/// Handles the User Agent header
+/// If the user agent is `curl` or `wget`,
+/// return a plain quote.
+/// Otherwise, return the root page.
+async fn handle_user_agent<T>(req: Request<T>) -> Response {
+    let header = Request::headers(&req);
+    let user_agent: String = if let Some(user_agent) = header.get(USER_AGENT) {
+        user_agent.clone().to_str().unwrap().to_string()
+    } else {
+        "blank".to_string()
+    };
+
+    info!("got user agent: {user_agent}");
+
+    if user_agent.contains("curl") || user_agent.contains("Wget") {
+        plain_quote().await
+    } else {
+        root().await
+    }
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -20,7 +41,7 @@ async fn main() -> anyhow::Result<()> {
     // Create a router
     info!("initializing router...");
     let app = Router::new()
-        .route("/", get(root))
+        .route("/", get(handle_user_agent))
         .route("/quote", get(quote));
 
     // run our app with hyper, listening globally on port 80
