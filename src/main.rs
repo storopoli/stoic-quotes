111b9b20d07df558e15899b78dc31da48e91c185
Jose Storopoli <jose@storopoli.io>
2024-01-12 07:01:55 -0300

    test: router and requests

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use tracing::Level;
+use anyhow::{Context, Result};
+use axum::serve;
+use tokio::net::TcpListener;
+use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
 
 mod app;
@@ -8,7 +11,7 @@ mod pages;
 use app::app;
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() -> Result<()> {
     // Set up logging
     let subscriber = FmtSubscriber::builder()
         // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
@@ -17,6 +20,15 @@ async fn main() -> anyhow::Result<()> {
         .finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 
-    app().await?;
+    let app = app();
+
+    // run our app with hyper, listening globally on port 80
+    // FIXME: use https port 443
+    let port = 80_u16;
+    let addr = TcpListener::bind(format!("0.0.0.0:{port}")).await.unwrap();
+    info!("router initialized, listening on port {:?}", port);
+    serve(addr, app)
+        .await
+        .context("error while starting server")?;
     Ok(())
 }
