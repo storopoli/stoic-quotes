10ff2be17c878918ba049b751dde6c744896ce12
Jose Storopoli <jose@storopoli.io>
2024-02-25 11:06:29 -0300

    fix: use &Quote instead of cloning Quote

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 //!
 //! `stoic-quotes` is a collection of stoic quotes in an Axum web server
 //! that serves stoic quotes with reactivity by the all-mighty
-//! [htmx](https://htmx.org) (no YAVASCRIPT).
+//! [`dioxus`](https://dioxuslabs.com) (no YAVASCRIPT!)
 //!
 //! It also has plain-text API GET endpoints at `/` that returns a stoic quote
 //! for terminal users with `curl` and `wget`.
